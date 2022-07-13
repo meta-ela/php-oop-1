3fcfafe7c0546c28eb74eaa735bc55351f1ba692
meta-ela <anxhelameta@live.it>
2022-07-13 18:27:56 +0200

    Adds class Movie echo

--- index.php
+++ index.php
@@ -9,14 +9,18 @@ class Movie {
 $movie1 = new Movie();
 $movie1->title = "Arrival";
 $movie1->author ="Denis Villeneuve";
-$movie1->year = "2016";
+$movie1->year = 2016;
 
 
 
 $movie2 = new Movie();
 $movie2->title = "Stoker";
 $movie2->author ="Park Chan-wook";
-$movie2->year = "2013";
+$movie2->year = 2013;
 
 var_dump($movie1);
 var_dump($movie2);
+
+echo "il film in lista da vedere è " . $movie1->title . ", prodotto nel " . $movie1->year . " e diretto da " . $movie1->author;
+echo "<br>";
+echo "il film in lista da vedere è " . $movie2->title . ", prodotto nel " . $movie2->year . " e diretto da " . $movie2->author;
